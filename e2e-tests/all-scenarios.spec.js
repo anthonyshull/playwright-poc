cf4a7e6d49f460786f84d80136d43cf7ffde2d5c
Anthony Shull <shull.anthony@gmail.com>
2024-01-22 13:32:43 -0600

    use special docker image

--- e2e-tests/all-scenarios.spec.js
+++ e2e-tests/all-scenarios.spec.js
@@ -7,6 +7,8 @@ const { test, expect } = require('@playwright/test');
 const filesPath = path.join(__dirname, '..', 'scenarios');
 const files = fs.readdirSync(filesPath);
 
+const baseURL = process.env.TARGET_URL;
+
 files.forEach((file) => {
     test.describe('All scenarios', () => {
         const filePath = path.join(filesPath, file);
@@ -15,7 +17,7 @@ files.forEach((file) => {
         test(scenario.name, async ({ page }) => {
             const start = performance.now();
 
-            await scenario.run({ page });
+            await scenario.run({ page, baseURL });
 
             const end = performance.now();
             const duration = end - start;
